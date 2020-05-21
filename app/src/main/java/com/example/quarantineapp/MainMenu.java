b00ba6a076e2a7d16e174a91f8618c2425bc644d
yena816 <yena816@gmail.com>
2020-05-21 12:25:57 -0400

    added some supplies page and linked to main menu

--- app/src/main/java/com/example/quarantineapp/MainMenu.java
+++ app/src/main/java/com/example/quarantineapp/MainMenu.java
@@ -13,11 +13,30 @@ public class MainMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainmenu);
 
+        //online games button
         Button button1 = findViewById(R.id.goToOnlineGames);
         button1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent2 = new Intent(MainMenu.this, OnlineGames.class);
                 startActivity(intent2);
+            }
+        });
+
+        //mobile games button
+        Button button2 = findViewById(R.id.goToMobileGames);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent3 = new Intent(MainMenu.this, MobileGames.class);
+                startActivity(intent3);
+            }
+        });
+
+        //some supplies button
+        Button button3 = findViewById(R.id.goToSomeSupplies);
+        button3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent4 = new Intent(MainMenu.this, SomeSupplies.class);
+                startActivity(intent4);
             }
         });
     }
---
+++ app/src/main/java/com/example/quarantineapp/MobileGames.java
@@ -0,0 +1,15 @@
+package com.example.quarantineapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MobileGames extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mobilegames);
+    }
+}
